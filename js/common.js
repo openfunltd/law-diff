83b2999ec1cf025c7fb14d0c1fbc65b6e33208f2
Boggy Jang <boggy@idrip.coffee>
2024-01-03 18:25:30 +0800

    update ui

--- js/common.js
+++ js/common.js
@@ -11,13 +11,34 @@ const common = {
     // ---------- 搜尋頁 ----------
     // 搜尋頁是否顯示進階搜尋
     const advancedSearchVisible = ref(false)
+    const searchConditions = ref([
+      {
+        title: '法律名稱關鍵字',
+        value: 'law_name'
+      },
+      {
+        title: '依照屆期會期搜尋',
+        value: 'period'
+      },
+      {
+        title: '依提案人名稱',
+        value: 'legislator'
+      },
+      {
+        title: '依立法進度搜尋',
+        value: 'progress'
+      }
+    ])
+    const searchBy = ref({})
+    searchBy.value = searchConditions.value[0]
+
+    const rwdFilterOptionsVisible = ref(false)
 
     // ---------- 議案頁 ----------
     // 議案頁的兩個對照版本 0為現行條文 -1為通過條文 其他請參考網頁中的versions
     const billViewing = ref('comparation') // 預設顯示法案對照表
 
-    const compareFrom = ref(0)
-    const compareBy = ref(-1)
+    const selectedVersions = ref([])
 
     // 議案頁版本修改者與修改內容
     const billVersions = ref([])
@@ -25,95 +46,56 @@ const common = {
 
     try {
       if (versions && versions.length) {
-        billVersions.value = versions.slice(1, versions.length)
+        billVersions.value = versions
       }
       if (bills && bills.length) {
         billsData.value = bills
       }
     } catch {}
 
-    // 產生比較法條列表
-    const filterBills = computed(() => {
-      if (!billVersions.value.length || !billsData.value.length) {
-        return
-      }
-
+    // 一目前選的版本，決定要顯示出哪幾條共同都有修改的條文
+    const allAvailableSections = computed (() => {
       const result = []
 
-      // 一條一條找有from（比較欄位左邊）與有by（比較欄位右邊）的資料
-      billsData.value.forEach(billsSet => {
-        const row = {
-          from: null,
-          by: null
+      billsData.value.map((item, idx) => {
+        return item.versions.filter((it, i) => {
+          return it.content && selectedVersions.value.includes(i)
+        }).length
+      }).forEach((item, idx) => {
+        if (item === selectedVersions.value.length) {
+          result.push(idx)
         }
-
-        const isNew = !billsSet[0].content
-
-        // 找左欄資料
-        if (billsSet[compareFrom.value].content) {
-          row.from = {
-            name: versions[compareFrom.value],
-            nowRule: compareFrom.value === 0,
-            new: isNew,
-            ...billsSet[compareFrom.value]
-          }
-        }
-
-        // 找右欄資料 當compareBy.value === -1時，要找審過(passed)的資料
-        if (compareBy.value === -1) {
-          const idx = billsSet.findIndex(bill => bill.passed)
-          row.by = idx > -1
-            ? {
-              name: versions[idx],
-              nowRule: false,
-              new: isNew,
-              ...billsSet[idx]
-            }
-            : null
-        } else if (billsSet[compareBy.value]?.content) {
-          row.by = {
-            name: versions[compareBy.value],
-            nowRule: false,
-            new: isNew,
-            ...billsSet[compareBy.value]
-          }
-        }
-
-        // 如果兩欄都有資料，製作diff顯示，只需要跟現行條文比較，不用做委員與委員之間的比較
-        // 只有其中一欄有資料的話，只要把\n轉br即可
-
-        // 現行條文
-        const nowBill = billsSet[0].content || ''
-        const diff = new Diff()
-        let textDiff
-
-        if (row.from) {
-          textDiff = diff.main(nowBill, row.from.content)
-          row.from.htmlContent = diff.prettyHtml(textDiff)
-          // row.from.htmlContent = row.from.content.replace(/\n/g, '<br>')
-        }
-
-        if (row.by) {
-          // row.by.htmlContent = '<ins>' + row.by.content.replace(/\n/g, '<br>') + '</ins>'
-          textDiff = diff.main(nowBill, row.by.content)
-          row.by.htmlContent = diff.prettyHtml(textDiff)
-        }
-
-        result.push(row)
       })
+
+      // 重新整理 selectedSections，若有不存在 allAvailableSections 的 Section 須移除
+      selectedSections.value = selectedSections.value.filter(item => result.includes(item))
 
       return result
     })
 
+    const selectedSections = ref([])
+
+    function compareDiff (content1, content2) {
+      const diff = new Diff()
+      let textDiff = diff.main(content1, content2)
+      return diff.prettyHtml(textDiff)
+    }
+
     return {
       rwdMenuVisible,
       advancedSearchVisible,
+      searchConditions,
+      searchBy,
+      rwdFilterOptionsVisible,
+
       billViewing,
-      compareFrom,
-      compareBy,
       billVersions,
       billsData,
-      filterBills
+      selectedVersions,
+      allAvailableSections,
+      selectedSections,
+
+      compareDiff
     }
   }
 }
